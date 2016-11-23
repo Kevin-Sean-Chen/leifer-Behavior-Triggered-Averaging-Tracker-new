97bd5048e47b9886273009a736efad5b1fd31404
Mochi Liu <youxi3013@126.com>
2016-11-23 15:21:48 -0500

    MATLAB jobID bugfix

--- call_function.m
+++ call_function.m
@@ -4,22 +4,22 @@ function success = call_function(input_string)
         inputs = strsplit(input_string,';');
         script_name = inputs{1};
         folder_name = inputs{2};
-        job_ID = get_job_ID(folder_name);
+        job_ID = get_job_ID(folder_name, script_name);
         disp(script_name);
         disp(folder_name);
         disp(job_ID);
         %update the logs before starting
-        update_logs(folder_name,script_name,'START',job_ID,'');
+        update_logs(folder_name,script_name,'START',job_ID,'Within_MATLAB');
         
         function_handle = str2func(script_name);
         %run the function
         function_handle(folder_name);
         success = true;
         %update the logs after completing
-        update_logs(folder_name,script_name,'COMPLETE',job_ID,'');
+        update_logs(folder_name,script_name,'COMPLETE',job_ID,'Within_MATLAB');
     catch ME
         success = false;
         %update the logs after completing
-        update_logs(folder_name,script_name,'ERROR',job_ID,regexprep(ME.message,'\r\n|\n|\r',' '));
+        update_logs(folder_name,script_name,'ERROR',job_ID,regexprep(ME.message,'\r\n|\n|\r','Within_MATLAB'));
     end
 end
--- get_job_ID.m
+++ get_job_ID.m
@@ -5,14 +5,15 @@ function job_ID = get_job_ID(folder_name, script_name)
         [~, last_log_line] = system(['tail -n 1 ', log_name]);
         if isstrprop(last_log_line(end), 'cntrl'), last_log_line(end) = []; end
         log_fields = strsplit(last_log_line,',');
-        if strcmp(log_fields{1},script_name) && strcmp(log_fields{1}, 'SUBMIT')
+        if strcmp(log_fields{1},script_name) && strcmp(log_fields{2},'SUBMIT')
             %last log entry matches the current one and was just submitted
             job_ID = log_fields{5};
         else
             job_ID = 'MATLAB';
         end
-    catch
+    catch ME
         % dont let not logging screw with execution
+        ME.message
         job_ID = 'MATLAB';
     end
 end
