4431a7daef4bb69dca2e9d0d4bf056b4229fadc8
mochiliu <youxi3013@126.com>
2016-12-14 11:01:52 -0500

    calculate_behaviors bugfix

--- calculate_behaviors.m
+++ calculate_behaviors.m
@@ -21,7 +21,7 @@ function success = calculate_behaviors(folder_name)
     Tracks(1).Behaviors = [];
 
     for track_index = 1:length(Tracks)
-        triggers = false(number_of_behaviors, length(Tracks(track_index).Frames)); %a binary array of when behaviors occur
+        triggers = false(number_of_behaviors, size(Tracks(track_index).Embeddings,1)); %a binary array of when behaviors occur
         for behavior_index = 1:number_of_behaviors
             transition_indecies = Tracks(track_index).BehavioralTransition(:,1) == behavior_index;
             %transition into of
@@ -31,7 +31,7 @@ function success = calculate_behaviors(folder_name)
 %                 transition_end_frames = Tracks(track_index).BehavioralTransition(transition_indecies,3);
 %                 triggers(behavior_index,transition_end_frames) = true;
         end
-        Tracks(track_index).Behaviors = triggers(:,1:length(Tracks(track_index).Frames));
+        Tracks(track_index).Behaviors = triggers(:,1:size(Tracks(track_index).Embeddings,1)));
     end
 
     %save
