d4c3d09439f0ef476cb474f7510c1c148e1ae0d5
mochiliu <youxi3013@126.com>
2016-12-14 10:58:22 -0500

    spectra param updated

--- Bash/ProcessExperimentDirectory.sh
+++ Bash/ProcessExperimentDirectory.sh
@@ -103,7 +103,7 @@ script_name=calculate_spectra
 script_order=$(ScriptToOrdering.sh $script_name)
 if [ "$step_to_start" -lt "$script_order" ]; then
 	#submit job to cluster
-	PROCESS_ID=$(sbatch -N1 -n6 --mem-per-cpu=8000M -t23:59:00 RunMatlabJVM.sh $folder_name $script_name) #
+	PROCESS_ID=$(sbatch -N1 -n6 --mem-per-cpu=8000M -t6:00:00 RunMatlabJVM.sh $folder_name $script_name) #
 	UpdateLog.sh $folder_name $script_name ${PROCESS_ID##* } SUBMIT Awaiting_Resources #update the log
 	while squeue -u $user_name | grep -q -w ${PROCESS_ID##* }; do sleep 10; done #wait until job finishes
 	#check if the operation completed succesfully
