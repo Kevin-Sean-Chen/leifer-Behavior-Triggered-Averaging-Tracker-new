4e92b44b976984952c6dee7703d07aaa801cf29b
Mochi Liu <youxi3013@126.com>
2017-01-20 15:53:38 -0500

    upping memory

--- Bash/ProcessExperimentDirectory.sh
+++ Bash/ProcessExperimentDirectory.sh
@@ -86,7 +86,7 @@ script_name=auto_resolve_problems
 script_order=$(ScriptToOrdering.sh $script_name)
 if [ "$step_to_start" -lt "$script_order" ]; then
 	#submit job to cluster
-	PROCESS_ID=$(sbatch -N1 -n1 --mem-per-cpu=4000M -t6:00:00 RunMatlabNoJVM.sh $folder_name $script_name) # 
+	PROCESS_ID=$(sbatch -N1 -n1 --mem-per-cpu=8000M -t6:00:00 RunMatlabNoJVM.sh $folder_name $script_name) # 
 	UpdateLog.sh $folder_name $script_name ${PROCESS_ID##* } SUBMIT Awaiting_Resources #update the log
 	while squeue -u $user_name | grep -q -w ${PROCESS_ID##* }; do sleep 10; done #wait until job finishes
 	#check if the operation completed succesfully
