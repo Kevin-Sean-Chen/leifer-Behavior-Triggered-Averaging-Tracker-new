b21cd216b976aea55ffbb536084e98f54e54e859
mochiliu <youxi3013@126.com>
2018-06-06 12:41:19 -0400

    3 behavior reference embedding

--- Scripts/turning_statistics.m
+++ Scripts/turning_statistics.m
@@ -1,5 +1,7 @@
 %% combine watersheds to generate behavior map for forward, reverse, turns
-combined_L = combine_watersheds(L, [7,8]);
-L = combine_watersheds(combined_L, [1,2,3,4,5,6]);
+L = combine_watersheds(L, [7,8]);
+L = combine_watersheds(L, [1,2,3,4,5,6]);
 behavior_names = {'Forward', 'Reverse', 'Turns'};
 behavior_colors = [1,0.466666666666667,0.466666666666667; 0.196078431372549,0.196078431372549,1; 0.854901960784314,0.00392156862745098,0.921568627450980];
+
+%% 
