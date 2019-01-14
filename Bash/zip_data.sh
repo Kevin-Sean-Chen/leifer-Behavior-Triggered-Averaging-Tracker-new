e6bb106e4fce7bbb3d952d2624b32c1d4d91ec95
Mochi Liu <youxi3013@126.com>
2019-01-14 15:00:20 -0500

    zip_data bash working

--- Bash/zip_data.sh
+++ Bash/zip_data.sh
@@ -5,11 +5,11 @@ job_ID=$2
 UpdateLog.sh $folder_name zip_data $job_ID START Zipping_Data_And_Deleting_Raw_Images
 
 #get how many jpg files are there
-jpg_count=$(ls *.jpg | wc -l)
+jpg_count=$(ls $folder_name/*.jpg | wc -l)
 
 if [ "$jpg_count" -gt "0" ]; then 
 	# jpg file exists, zip them
-	zip -m -u raw_images.zip *.jpg
+	zip -m -u $folder_name/raw_images.zip $folder_name/*.jpg
 fi
 
 UpdateLog.sh $folder_name delete_tracks $job_ID COMPLETE $jpg_count'jpg_files_zipped'
