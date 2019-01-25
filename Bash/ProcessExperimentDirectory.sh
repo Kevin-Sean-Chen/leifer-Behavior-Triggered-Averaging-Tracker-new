b79f2dd8de8ca67975adf8dbc8794556ab6f4a9c
mochiliu <youxi3013@126.com>
2019-01-25 13:15:26 -0500

    even more memory

--- Bash/ProcessExperimentDirectory.sh
+++ Bash/ProcessExperimentDirectory.sh
@@ -66,7 +66,7 @@ script_name=track_image_directory
 script_order=$(ScriptToOrdering.sh $script_name)
 if [ "$step_to_start" -lt "$script_order" ]; then
 	#submit job to cluster
-	PROCESS_ID=$(sbatch -N1 -n1 --mem-per-cpu=8000M -t23:59:00 RunMatlabNoJVM.sh $folder_name $script_name) #  
+	PROCESS_ID=$(sbatch -N1 -n1 --mem-per-cpu=12000M -t23:59:00 RunMatlabNoJVM.sh $folder_name $script_name) #  
 	UpdateLog.sh $folder_name $script_name ${PROCESS_ID##* } SUBMIT Awaiting_Resources #update the log
 	while squeue -u $user_name | grep -q -w ${PROCESS_ID##* }; do sleep 10; done #wait until job finishes
 	#check if the operation completed succesfully
