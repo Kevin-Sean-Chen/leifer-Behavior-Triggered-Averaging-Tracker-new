8a60668570b29c30b9725df045fdf5a49f9ff59a
mochiliu <youxi3013@126.com>
2019-05-28 15:10:01 -0400

    upped find centerline memory

--- ProcessDateDirectory.m
+++ ProcessDateDirectory.m
@@ -1,8 +1,8 @@
 % This script performs behavioral analysis without a cluster
 
 % analysis options
-tracking = 1;
-finding_centerline = 0;
+tracking = 0;
+finding_centerline = 1;
 resolving_problems = 0;
 plotting = 0;
 calculate_behavior = 0;
