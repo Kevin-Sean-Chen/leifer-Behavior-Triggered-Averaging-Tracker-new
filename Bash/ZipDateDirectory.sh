f24577cab5bf5a4061a4b75cc65cc81b8ae93766
mochiliu <youxi3013@126.com>
2019-06-04 14:48:42 -0400

    simplified zip experiment directory

--- Bash/ZipDateDirectory.sh
+++ Bash/ZipDateDirectory.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 date_folder=$1
-start_point=$2
-next_step=$(($start_point + 1))
-
 
 
 if [ -f "$date_folder/parameters.txt" ]; then
 	# this is a experiment folder
-	ZipExperimentDirectory.sh $date_folder $start_point &
+	ZipExperimentDirectory.sh $date_folder &
 else
 	# this is not an experiment folder, repeat iteratively
 	# loop through all the directories in a date folder
@@ -17,7 +14,7 @@ else
 	    if [ -d ${folder_name} ]; then
 	    	# this is a directory
 	        # echo $folder_name/
-	        ZipDateDirectory.sh $folder_name/ $start_point &
+	        ZipDateDirectory.sh $folder_name/ &
 	    fi
 	done
 fi
