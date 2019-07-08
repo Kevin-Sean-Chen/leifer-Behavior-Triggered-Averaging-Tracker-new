627807c981eccde6a78b9df5ac9e6961273ccdb3
mochiliu <youxi3013@126.com>
2019-07-08 17:34:37 -0400

    added unzip bash scripts

--- Scripts/custom_tap_stimulus_velocity_analysis.m
+++ Scripts/custom_tap_stimulus_velocity_analysis.m
@@ -105,32 +105,79 @@ for condition_index = 1:length(conditions)
     ylabel('Velocity Before Tap (mm/s)')
     title(conditions{condition_index})
     
-    %delta V based pie charts
+    %classify each track
+    transition_classification = cell(1, length(velocities_before));
+    for track_index = 1:length(velocities_before)
+       delta_velocity = velocities_after(track_index) - velocities_before(track_index);
+       if velocities_before(track_index) < 0
+           if velocities_after(track_index) > 0
+               transition_classification{track_index} = 'Rev to Fwd';
+           elseif delta_velocity < -thresh_velocity
+               transition_classification{track_index} = 'Rev to Faster Rev';
+           elseif delta_velocity > thresh_velocity
+               transition_classification{track_index} = 'Rev to Slowed Rev';
+           else
+               transition_classification{track_index} = 'Rev - Same';
+           end
+       else
+           if velocities_after(track_index) < 0
+               transition_classification{track_index} = 'Fwd to Rev';
+           elseif delta_velocity < -thresh_velocity
+               transition_classification{track_index} = 'Fwd to Slowed Fwd';
+           elseif delta_velocity > thresh_velocity
+               transition_classification{track_index} = 'Fwd to Faster Fwd';
+           else
+               transition_classification{track_index} = 'Fwd - Same';
+           end
+       end
+    end
     
-    %exclude when velocity is < 0 before
-    excluded_indecies_because_animal_was_reversing = velocities_before < 0;
-    velocities_before(excluded_indecies_because_animal_was_reversing) = [];
-    velocities_after(excluded_indecies_because_animal_was_reversing) = [];
-    reverse_before_tap_count = sum(excluded_indecies_because_animal_was_reversing);
     
-    excluded_indecies_because_animal_reversed_after_tap = velocities_after < 0;
-    velocities_before(excluded_indecies_because_animal_reversed_after_tap) = [];
-    velocities_after(excluded_indecies_because_animal_reversed_after_tap) = [];
-    reverse_after_tap_count = sum(excluded_indecies_because_animal_reversed_after_tap);
+    %let's count this
+    behavior_types = {'Rev to Fwd', 'Rev to Faster Rev', 'Rev to Slowed Rev', 'Rev - Same', 'Fwd to Rev', 'Fwd to Slowed Fwd', 'Fwd to Faster Fwd', 'Fwd - Same'};
+    behavior_counts = zeros(1, length(behavior_types));
+    for track_index = 1:length(transition_classification)
+        [~,behavior_index] = ismember(transition_classification{track_index}, behavior_types);
+        if behavior_index < 1
+            %not found
+            behavior_types = [behavior_types, transition_classification{track_index}];
+            behavior_counts = [behavior_counts, 1];
+        else
+            behavior_counts(behavior_index) = behavior_counts(behavior_index) + 1;
+        end
+    end
     
-    delta_velocity = velocities_after - velocities_before;
+    total_count = length(transition_classification);
     
-    slowdown_count = sum(delta_velocity < -thresh_velocity);
-    speedup_count = sum(delta_velocity > thresh_velocity);
-    same_count = length(delta_velocity) - slowdown_count - speedup_count;
-    total_count = reverse_after_tap_count + slowdown_count + speedup_count + same_count;
+    pie_labels = behavior_types;
+    for behavior_index = 1:length(pie_labels)
+        pie_labels{behavior_index} = [pie_labels{behavior_index}, ' ', num2str(round(behavior_counts(behavior_index)/total_count*100)),'% (n=', num2str(behavior_counts(behavior_index)),')'];
+    end
     
-    pie_labels = {['Reverse ', num2str(round(reverse_after_tap_count/total_count*100)),'% (n=', num2str(reverse_after_tap_count),')'], ...
-        ['Same ', num2str(round(same_count/total_count*100)),'% (n=', num2str(same_count),')'], ...
-        ['Speed Up ', num2str(round(speedup_count/total_count*100)),'% (n=', num2str(speedup_count),')'], ...
-        ['Slow Down ', num2str(round(slowdown_count/total_count*100)),'% (n=', num2str(slowdown_count),')']};
-    explode = [1 1 1 1];
+    explode = ones(1,length(behavior_types));
     figure
-    pie([reverse_after_tap_count, same_count, speedup_count, slowdown_count], explode, pie_labels);
+    pie(behavior_counts, explode, pie_labels);
     title(conditions{condition_index})
 end
+
+
+%     %exclude when velocity is < 0 before
+%     excluded_indecies_because_animal_was_reversing = velocities_before < 0;
+%     velocities_before(excluded_indecies_because_animal_was_reversing) = [];
+%     velocities_after(excluded_indecies_because_animal_was_reversing) = [];
+%     reverse_before_tap_count = sum(excluded_indecies_because_animal_was_reversing);
+%     
+%     excluded_indecies_because_animal_reversed_after_tap = velocities_after < 0;
+%     velocities_before(excluded_indecies_because_animal_reversed_after_tap) = [];
+%     velocities_after(excluded_indecies_because_animal_reversed_after_tap) = [];
+%     reverse_after_tap_count = sum(excluded_indecies_because_animal_reversed_after_tap);
+%     
+%     delta_velocity = velocities_after - velocities_before;
+%     slowdown_count = sum(delta_velocity < -thresh_velocity);
+%     speedup_count = sum(delta_velocity > thresh_velocity);
+%     same_count = length(delta_velocity) - slowdown_count - speedup_count;
+%     total_count = reverse_after_tap_count + slowdown_count + speedup_count + same_count;    
+% pie_labels = {['Reverse ', num2str(round(reverse_after_tap_count/total_count*100)),'% (n=', num2str(reverse_after_tap_count),')'], ...
+%         ['Same ', num2str(round(same_count/total_count*100)),'% (n=', num2str(same_count),')'], ...
+%         ['Speed Up ', num2str(round(speedup_count/total_count*100)),'% (n=', num2str(speedup_count),')'], ...
+%         ['Slow Down ', num2str(round(slowdown_count/total_count*100)),'% (n=', num2str(slowdown_count),')']};
--- Scripts/custom_tap_stimulus_video_analysis.m
+++ Scripts/custom_tap_stimulus_video_analysis.m
@@ -1,3 +1,77 @@
+
+%load tracks
+relevant_track_fields = {'Path','Frames','Velocity'};
+
+%select folders
+folders_platetap = getfoldersGUI();
+
+%load stimuli.txt from the first experiment
+num_stimuli = 1;
+fps = 14;
+normalized_stimuli = 1; %delta function
+window_size = 2*fps;
+time_window_before = window_size;
+time_window_after = window_size;
+
+n_bins = 20;
+edges = linspace(-0.3,0.3,n_bins);
+
+conditions = {'Tap', 'Control'};
+top_percentile_cutoff = 95;
+
+output_number_of_tracks = 100; % this is the number of tracks we are looking for
+
+%% behavioral rate compare
+
+%for the first experiment, search for the occurance of each stimulus after
+%normalizing to 1
+LEDVoltages = load([folders_platetap{1}, filesep, 'LEDVoltages.txt']);
+%LEDVoltages = LEDVoltages(randperm(length(LEDVoltages))); %optional, randomly permuate the taps
+LEDVoltages(LEDVoltages>0) = 1; %optional, make the stimulus on/off binary
+
+%find when each stimuli is played back by convolving the time
+%reversed stimulus (cross-correlation)
+xcorr_ledvoltages_stimulus = padded_conv(LEDVoltages, normalized_stimuli);
+peak_thresh = 0.99.*max(xcorr_ledvoltages_stimulus); %the peak threshold is 99% of the max (because edge effects)
+[~, tap_frames] = findpeaks(xcorr_ledvoltages_stimulus, 'MinPeakHeight', peak_thresh,'MinPeakDistance',14);
+
+%generate a series of control taps
+control_frame_shift = round((tap_frames(2)-tap_frames(1))/2); %the control taps are exactly in between taps
+control_LEDVoltages = circshift(LEDVoltages,[0,control_frame_shift]);
+xcorr_ledvoltages_stimulus = padded_conv(control_LEDVoltages, normalized_stimuli);
+[~, control_frames] = findpeaks(xcorr_ledvoltages_stimulus, 'MinPeakHeight', peak_thresh,'MinPeakDistance',14);
+
+
+%% calculate the threshold velocity
+allTracks = [];
+for folder_index = 1:length(folders_platetap)
+    %load the tracks for this folder
+    [current_tracks, folder_indecies_revstim_ret, track_indecies_revstim_ret] = loadtracks(folders_platetap{folder_index},{'Velocity'});
+    allTracks = [allTracks, current_tracks];
+end
+
+boxcar_window = ones(1,time_window_before+time_window_after+1) ./ (time_window_before+time_window_after+1);
+% calculate all possible velocities to get the threshold
+all_delta_velocities = [];
+for track_index = 1:length(allTracks)
+    boxcar_velocity = padded_conv(allTracks(track_index).Velocity, boxcar_window);
+    delta_veolocities = boxcar_velocity((time_window_before+time_window_after+1):end) - boxcar_velocity(1:end-(time_window_before+time_window_after));
+    all_delta_velocities = [all_delta_velocities, delta_veolocities];
+end
+thresh_velocity = prctile(all_delta_velocities, top_percentile_cutoff);
+
+%% allow for random sampling of tap events
+current_index = 1;
+possible_taps_to_sample = [];
+for folder_index = 1:length(folders_platetap)
+    for tap_frame_index = 1:length(tap_frames)
+        possible_taps_to_sample(current_index).folder = folders_platetap{folder_index};
+        possible_taps_to_sample(current_index).tap_frame = tap_frames(tap_frame_index);
+        current_index = current_index + 1;
+    end
+end
+
+%% draw it out
 % Setup figure for plotting tracker results
 % -----------------------------------------
 WTFigH = findobj('Tag', 'WTFIG');
@@ -77,7 +151,7 @@ while current_index < output_number_of_tracks
     % lets plot a video with this window!
     for frame_index = sampled_tap_frame-time_window_before-1:sampled_tap_frame+time_window_after
         % Get Frame
-        curImage = imread([sampled_folder_name, filesep, image_files(frame_index).name]);
+        curImage = imread([folder_name, filesep, image_files(frame_index).name]);
         imshow(curImage,'InitialMagnification',100, 'Border','tight');
         hold on;
         if ~isempty(Tracks)
