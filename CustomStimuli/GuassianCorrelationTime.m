49e0d4e59171c6374fe768cdffffd0f9740dbf38
mochiliu <youxi3013@126.com>
2019-09-26 16:18:15 -0400

    tap+light
    Add vis that can tap and shine light and record at the same time.

--- CustomStimuli/GuassianCorrelationTime.m
+++ CustomStimuli/GuassianCorrelationTime.m
@@ -23,29 +23,29 @@ function powers = GuassianCorrelationTime()
     powers(powers>maxPower) = maxPower;
     %voltages = powers / VoltageToPower; %convert to voltages
 
-    % figure
-    % plot(0:1/fps:(frame_count-1)/fps, powers, 'bo-')
-    % xlabel('time (s)') % x-axis label
-    % ylabel('voltage') % y-axis label
-    % 
-    % 
-    % N = length(powers);
-    % xdft = fft(powers);
-    % xdft = xdft(1:N/2+1);
-    % psdx = (1/(frame_count*N)) * abs(xdft).^2;
-    % psdx(2:end-1) = 2*psdx(2:end-1);
-    % freq = 0:frame_count/length(powers):frame_count/2;
+    figure
+    plot(0:1/frame_rate:(frame_count-1)/frame_rate, powers, 'bo-')
+    xlabel('time (s)') % x-axis label
+    ylabel('voltage') % y-axis label
+    
+    
+    N = length(powers);
+    xdft = fft(powers);
+    xdft = xdft(1:N/2+1);
+    psdx = (1/(frame_count*N)) * abs(xdft).^2;
+    psdx(2:end-1) = 2*psdx(2:end-1);
+    freq = 0:frame_count/length(powers):frame_count/2;
 
-    % figure;
-    % plot(freq(1:100),10*log10(psdx(1:100)))
-    % grid on
-    % title('Periodogram Using FFT')
-    % xlabel('Frequency (Hz)')
-    % ylabel('Power/Frequency (dB/Hz)')
+    figure;
+    plot(freq(1:100),10*log10(psdx(1:100)))
+    grid on
+    title('Periodogram Using FFT')
+    xlabel('Frequency (Hz)')
+    ylabel('Power/Frequency (dB/Hz)')
 
-    % periodogram(powers,rectwin(length(powers)),length(powers),fps, 'power')
-    % 
-    % figure 
-    % autocorr(powers)
-    % xlabel('frames (at 14fps)')
+    periodogram(powers,rectwin(length(powers)),length(powers),frame_rate, 'power')
+    
+    figure 
+    autocorr(powers)
+    xlabel('frames (at 14fps)')
 end
