bb7518b7f56f1fa3df262cfd59a1fc9c3b9907af
Yuhang_computer1 <yuhangc@princeton.edu>
2019-10-14 15:06:36 -0400

    parameter update

--- LabviewVIs/mix_tap_light_stimuli.m
+++ LabviewVIs/mix_tap_light_stimuli.m
@@ -1,61 +1,68 @@
-
+close all
 % Those are the UI inputs
 
 dur=280;
 period=28;
-power=10;
-sti=14;
+minPower=4;
+maxPower=10;
+sti=25;
 hold_before_tap=5;
-taps=2;
-prop_tap=0.3;
-prop_light=0.3;
-
+taps=1;
+prop_no_tap=0.2;
+prop_no_light=0.3;
 
 % powers are for LED light; voltages are for tapping
 % sti is the length of stimuli (ligh is on)
-% by default, taps are applied in the middle of light pulse
-% taps can wait for additional frames with hold_before_tap
-vol_tap=5; 
+% An array of power is generated based on the number
+% of trials we have for the whole duration
+powers=zeros(1,dur);
+voltages=zeros(1,dur);
+vol_tap=5;
 cycle=floor(dur/period);
-number_only_tap=round(prop_tap*cycle);
-number_only_light=round(prop_light*cycle);
-remain=mod(dur,period);
+number_tap=round((1-prop_no_tap)*cycle);
+number_light=round((1-prop_no_light)*cycle);
+number_both=round((1-prop_no_light-prop_no_tap)*cycle);
+number_only_light=number_light-number_both;
+%create randomly shuffled power array
+power_array=linspace(minPower,maxPower,number_both);
+
+% power and voltage in one period
 ini_sti=period-sti+1;
-tap_int=(sti-hold_before_tap)/(taps+1); %tap intervals
+tap_int=(sti-hold_before_tap)/(taps); %tap intervals
 p_per=zeros(1,period);
-v_per=zeros(1,period);%power and voltage in one period
-
-% the power and voltage of one standard period
-for i=ini_sti:period  
-    p_per(i)=power;
+v_per=zeros(1,period);
+p_per(ini_sti:period)=1;
+for i =1:taps % tap_voltages in one standard period
+    v_per(ini_sti+hold_before_tap+round(tap_int*(i-1)))=vol_tap;
 end
-for i =1:taps  % add hold_before_tap in each sti
-    v_per(ini_sti+hold_before_tap+round(tap_int*i))=vol_tap; 
-end
-% first assign each cycle with both light and tap
-powers=repmat(p_per,1,cycle);
-powers=[powers,p_per(1:remain)]; 
-voltages =repmat(v_per,1,cycle);
-voltages=[voltages,v_per(1:remain)];
-
-% randomly select which cycles to apply conditions 
-rand_ind_cycle=randperm(cycle,number_only_light+number_only_tap);
-cycle_index_WO_light=rand_ind_cycle(1:number_only_tap);
-cycle_index_WO_tap=rand_ind_cycle(number_only_tap+1:end);
-%modify the cycles without light
-for i=cycle_index_WO_light
-    end_frame=i*period;
+% randomly select which cycles to apply conditions
+rand_ind_cycle=randperm(cycle);
+light_only_index=rand_ind_cycle(1:number_only_light);
+both_index=rand_ind_cycle(number_only_light+1:number_light);
+tap_index=rand_ind_cycle(cycle-number_tap+1:end);
+for i=1:number_tap
+    cycle_inx=tap_index(i);
+    end_frame=cycle_inx*period;
     ini_frame=end_frame-period+1;
-    powers(ini_frame:end_frame)=0;
+    voltages(ini_frame:end_frame)=v_per;
 end
-%modify the cycles without tap
-for i=cycle_index_WO_tap
-    end_frame=i*period;
+for i=1:number_both
+    cycle_inx=both_index(i);
+    power=power_array(i);
+    end_frame=cycle_inx*period;
     ini_frame=end_frame-period+1;
-    voltages(ini_frame:end_frame)=0;
+    powers(ini_frame:end_frame)=p_per.*power;
 end
-
+%for light only cycles, we use maxPower
+for i=1:number_only_light
+    cycle_inx=light_only_index(i);
+    power=maxPower;
+    end_frame=cycle_inx*period;
+    ini_frame=end_frame-period+1;
+    powers(ini_frame:end_frame)=p_per.*power;
+end
 %%
+figure
 xx=1:dur;
 plot(xx,powers);hold on
 plot(xx,voltages)
