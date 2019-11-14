8c742770db1b60d81e7147acafcbf23e39741b0b
cyhFlight <32890805+cyhFlight@users.noreply.github.com>
2019-11-14 15:56:32 -0500

    fix for LED_only and Tap_only tracks

--- Scripts/custom_optotap_stimulus_script.m
+++ Scripts/custom_optotap_stimulus_script.m
@@ -5,6 +5,7 @@ relevant_track_fields = {'BehavioralTransition','Frames','TapVoltages','LEDVolta
 %select folders
 folders_optotap = getfoldersGUI();
 
+% specify tracks of stimuli condition
 LED_and_Tap=1;
 LED_only=0;
 Tap_only=0;
@@ -35,14 +36,14 @@ for folder_index = 1:length(folders_optotap)
     % [sti_tracks,TAPonly_tracks,LEDonly_tracks]=get_tracks_with_sti(folder_tracks);
     %specify which conditions to analyze
     current_tracks=folder_tracks;
-
+    
     current_param = load_parameters(folders_optotap{folder_index});
     LEDVoltages = load([folders_optotap{folder_index}, filesep, 'LEDVoltages.txt']);
     TapVoltages = load([folders_optotap{folder_index}, filesep, 'TapVoltages.txt']);
     %convert LEDVoltages to power
     % uses the parameter.csv data
     %LEDPowers = round(LEDVoltages .* current_param.avgPower500 ./ 5);
- 
+    
     % Use the parameters.txt (LabView input data)
     folder_paras=readtable([folders_optotap{folder_index}, filesep, 'parameters.txt'],'Delimiter','\t');
     LEDPowers = round(LEDVoltages.*folder_paras.VoltageToPower);
@@ -52,6 +53,7 @@ for folder_index = 1:length(folders_optotap)
     xcorr_ledvoltages_stimulus = padded_conv(LEDPowers, normalized_stimuli);
     [~, LED_peaks] = findpeaks(xcorr_ledvoltages_stimulus, 'MinPeakDistance',14);
     
+    % find stimuli time point based on conditions
     Tap_on=TapVoltages>0;
     LED_on=LEDVoltages>0;
     both_on=Tap_on&LED_on;
@@ -64,24 +66,25 @@ for folder_index = 1:length(folders_optotap)
         peak_locations=find(Tap_on&(~LED_on))-LED_before_tap;
     end
     
-    %loop through the peaks and cut up tracks
+    %loop through the peaks to update light powers
     for peak_index = 1:length(peak_locations)
-        %get the stimulus intensity for this peak
-        current_stim_power = LEDPowers(peak_locations(peak_index));
-        %see if this stim_power already exists
-        current_stim_index = find(and(stimulus_intensities < current_stim_power*stim_similarity_thresh,stimulus_intensities > current_stim_power/stim_similarity_thresh));
-        if isempty(current_stim_index)
-            %no entry yet
-            stimulus_intensities = [stimulus_intensities,current_stim_power];
-            current_stim_index = length(stimulus_intensities);
-            all_behavior_transitions_for_frame{current_stim_index} = cell(1,total_window_frames);
-            all_behavior_annotations_for_frame{current_stim_index} = cell(1,total_window_frames);
+        if Tap_only
+            stimulus_intensities=0;
+            all_behavior_transitions_for_frame{1} = cell(1,total_window_frames);
+            all_behavior_annotations_for_frame{1} = cell(1,total_window_frames);
+            
+        else  %get the stimulus intensity for this peak
+            current_stim_power = LEDPowers(peak_locations(peak_index));
+            %see if this stim_power already exists
+            current_stim_index = find(and(stimulus_intensities < current_stim_power*stim_similarity_thresh,stimulus_intensities > current_stim_power/stim_similarity_thresh));
+            if isempty(current_stim_index)
+                %no entry yet
+                stimulus_intensities = [stimulus_intensities,current_stim_power];
+                current_stim_index = length(stimulus_intensities);
+                all_behavior_transitions_for_frame{current_stim_index} = cell(1,total_window_frames);
+                all_behavior_annotations_for_frame{current_stim_index} = cell(1,total_window_frames);
+            end
         end
-        
-        
-        % delete the tracks with no taps, based on the fact that the peaks
-        % are when the LED is triggered, and the tap is delivered 2 seconds
-        % after
         
         %for every time a stimulus is delivered, look at a certain range of
         %frames
@@ -89,6 +92,7 @@ for folder_index = 1:length(folders_optotap)
             current_frame = peak_locations(peak_index) +LED_before_tap +frame_shift;
             if current_frame <= length(LEDPowers) && current_frame >= 1
                 %make sure the current frame is in range
+                %cut up tracks to each frame
                 tracks_on_critical_frame = FilterTracksByTime(current_tracks,current_frame, current_frame);
                 if ~isempty(tracks_on_critical_frame)
                     all_behavior_transitions_for_frame{current_stim_index}{frame_shift+time_window_before+1} = [all_behavior_transitions_for_frame{current_stim_index}{frame_shift+time_window_before+1}, tracks_on_critical_frame.Behaviors];
@@ -116,10 +120,10 @@ all_behavior_annotations_for_frame= all_behavior_annotations_for_frame(sort_inde
 %         transition_rate_for_frame(:,frame_index) = sum(transitions_for_frame,2)./size(transitions_for_frame,2).*fps.*60;
 %         transition_std_for_frame(:,frame_index) = sqrt(sum(transitions_for_frame,2))./size(transitions_for_frame,2).*fps.*60;
 %     end
-% 
+%
 %     track_n = round(mean(arrayfun(@(x) size(x{1},2), [all_behavior_transitions_for_frame{stimulus_index}])));
 %     my_colors = behavior_colors;
-%    
+%
 %     figure
 %     hold on
 %     for behavior_index = 1:number_of_behaviors
@@ -135,7 +139,7 @@ all_behavior_annotations_for_frame= all_behavior_annotations_for_frame(sort_inde
 %     ax.FontSize = 10;
 %     axis([-10 10 0 35])
 % end
-%% 2 plot the behavioral ratios as a function of time
+
 behavior_counts_for_frame = zeros(number_of_behaviors,length(stimulus_intensities),total_window_frames);
 behavior_ratios_for_frame = zeros(number_of_behaviors,length(stimulus_intensities),total_window_frames);
 
@@ -149,9 +153,9 @@ for stimulus_index = 1:length(stimulus_intensities)
         behavior_ratios_for_frame(:,stimulus_index,frame_index) = behavior_counts_for_frame(:,stimulus_index,frame_index)./sum(behavior_counts_for_frame(:,stimulus_index,frame_index)); %get ratio
     end
 end
-
+%% 2 plot the behavioral ratios as a function of time
 for stimulus_index = 1:length(stimulus_intensities)
-    track_n = round(mean(arrayfun(@(x) size(x{1},2), [all_behavior_transitions_for_frame{stimulus_index}])));
+    track_n = round(mean(arrayfun(@(x) size(x{1},2), [all_behavior_transitions_for_frame{1,stimulus_index}])));
     my_colors = behavior_colors;
     figure
     hold on
@@ -162,27 +166,27 @@ for stimulus_index = 1:length(stimulus_intensities)
     xlabel('Time (s)') % x-axis label
     ylabel('Behavioral Ratio') % y-axis label
     title(['Stimulus Intensity = ', num2str(stimulus_intensities(stimulus_index)), ' (n = ', num2str(track_n), ' tracks)']);
-
+    
     legend('show');
     ax = gca;
     ax.FontSize = 10;
 end
 axis([-time_window_before/fps time_window_after/fps 0 0.7])
-% 
+%
 %% plot the behavioral ratios for various intensities on the same plot
 for behavior_index = [3,8,9] %fast forward3; fast reverse; turns
     my_colors = lines(length(stimulus_intensities));
     figure
     hold on
     for stimulus_index = 1:length(stimulus_intensities)
-        track_n = round(mean(arrayfun(@(x) size(x{1},2), [all_behavior_transitions_for_frame{stimulus_index}])));
+        track_n = round(mean(arrayfun(@(x) size(x{1},2), [all_behavior_transitions_for_frame{1,stimulus_index}])));
         plot(-time_window_before/fps:1/fps:time_window_after/fps, squeeze(behavior_ratios_for_frame(behavior_index,stimulus_index,:)), '-', 'color', my_colors(stimulus_index,:),'Linewidth', 3,'DisplayName',[num2str(stimulus_intensities(stimulus_index)), 'uW/mm2 (n = ', num2str(track_n),' tracks)']);
     end
     hold off
     xlabel('Time (s)') % x-axis label
     ylabel('Behavioral Ratio') % y-axis label
     title(behavior_names{behavior_index});
-
+    
     legend('show','Location','northwest');
     ax = gca;
     ax.FontSize = 10;
@@ -197,26 +201,23 @@ x = stimulus_intensities;
 err_boot=zeros(1,length(x));
 % standard error of the mean
 sem=zeros(1,length(x));
+% find the max ratio within the time window
 [y2,mi]= max(behavior_ratios_for_frame(behavior_index,:,:),[],3);
 
 for stimulus_index = 1:length(stimulus_intensities)
     % create binary data at the frame when the ratio of behavior is max
-binary_data=(all_behavior_annotations_for_frame{stimulus_index}{mi(stimulus_index)}==behavior_index);
-% get standard error from bootstraped data
-% bootratio=bootstrp(20000,@mean,binary_data); 
-% err_boot(stimulus_index)=std(bootratio);
-% mathematical formula of standard error of the mean
-sem(stimulus_index)=std(binary_data)/sqrt(length(binary_data));
+    binary_data=(all_behavior_annotations_for_frame{stimulus_index}{mi(stimulus_index)}==behavior_index);
+    % get standard error from bootstraped data
+    % bootratio=bootstrp(20000,@mean,binary_data);
+    % err_boot(stimulus_index)=std(bootratio);
+    % mathematical formula of standard error of the mean
+    sem(stimulus_index)=std(binary_data)/sqrt(length(binary_data));
 end
 
 figure('Position',[100,100,800,600])
-% hold on
-% rectangle('Position',[0,0,50,0.6],'FaceColor',[1 0.5 0.5])
-% plot(x, y, 'ro-', 'LineWidth',2,'Markersize',10)
-% hold on
 errorbar(x, y2,sem, 'bo-', 'LineWidth',2,'Markersize',10)
 for stimulus_index = 1:length(stimulus_intensities)
-    track_n = round(mean(arrayfun(@(x) size(x{1},2), [all_behavior_transitions_for_frame{stimulus_index}])));
+    track_n = round(mean(arrayfun(@(x) size(x{1},2), [all_behavior_transitions_for_frame{1,stimulus_index}])));
     text(x(stimulus_index), y2(stimulus_index), ['   n=', num2str(track_n)]);
 end
 ax = gca;
